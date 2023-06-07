4921600d4e89767b6dbb35893bfa938ee12f236b
shivateja <shiva.tejas99@gmail.com>
2023-06-07 11:15:58 +0530

    subt commit

---
+++ src/com/cisco/subt.java
@@ -0,0 +1,19 @@
+package com.cisco;
+
+public class subt {
+	public void subtraction() {
+		
+		int a =15;
+		int b =26;
+		int c = a+b;
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		System.out.println("a+b = c ="+c);
+		
+	}
+	public static void main(String[] args) {
+		subt s = new subt();
+		s.subtraction();
+	}
+
+}
